9dc450a9ba77e42223ee6400f2b8dc2f4a948739
minikocha <minikocha@gmail.com>
2025-10-27 13:25:23 +0900

    fix to use existing method

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -17,14 +17,14 @@ year  = Date.today.year if year == nil
 month = Date.today.month if month == nil
 
 # ヘッダーの表示
-puts "      #{" " if month < 10}#{month}月 #{year}"
+puts "      #{month.to_s.rjust(2)}月 #{year}"
 puts "日 月 火 水 木 金 土"
 
 i = Date.new(year, month, 1).cwday
 i = 0 if i == 7                                # NOTE:日曜日の場合は横軸の位置をリセットする
 print "#{"   " * i}"                           # NOTE: 右寄せのためのマージンをprint
 (1..Date.new(year, month, -1).day).each do |d|
-  print "#{" " if d < 10}#{d}"
+  print d.to_s.rjust(2)
   i +=1
   if i > 6
     puts ""
