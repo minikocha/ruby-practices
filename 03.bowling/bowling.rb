9682ca8410091bc2e1b89e7d9e6b59f30cdbdbd4
minikocha <minikocha@gmail.com>
2025-10-29 17:27:24 +0900

    fix converting the same result to a number multiple times

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,18 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# NOTE: 文字列として渡されたピンの数を数字に変換する。
-def convert_to_number(pins)
-  return 10 if pins == 'X'
-
-  pins.to_i
+# NOTE: 引数として渡された文字列（結果）を数値の配列に変換する。
+def convert_to_numbers(arg)
+  results = []
+  arg.split(',').each do |result|
+    if result == 'X'
+      results.append(10)
+    else
+      results.append(result.to_i)
+    end
+  end
+  results
 end
 
 # NOTE: 1フレーム毎のポイントを計算し、結果と次回以降の読み込み開始の位置を返却する。
 def calculate_point(results, position)
-  first  = convert_to_number(results[position])
-  second = convert_to_number(results[position + 1])
-  third  = convert_to_number(results[position + 2])
+  first  = results[position]
+  second = results[position + 1]
+  third  = results[position + 2]
 
   return [(first + second + third), (position + 1)] if first == 10
 
@@ -21,7 +27,7 @@ def calculate_point(results, position)
   [point, (position + 2)]
 end
 
-results  = ARGV[0].split(',')
+results  = convert_to_numbers(ARGV[0])
 score    = 0
 position = 0
 10.times do
