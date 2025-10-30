f4c26288ec1f3cbb2b761f0ecf587f2f1f21ef77
minikocha <minikocha@gmail.com>
2025-10-30 13:39:11 +0900

    simplify the code

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,38 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# NOTE: 引数として渡された文字列（結果）を数値の配列に変換する。
-def convert_to_numbers(arg)
-  results = []
-  arg.split(',').each do |result|
-    if result == 'X'
-      results.append(10)
-    else
-      results.append(result.to_i)
-    end
-  end
-  results
-end
-
-# NOTE: 1フレーム毎のポイントを計算し、結果と次回以降の読み込み開始の位置を返却する。
-def calculate_point(results, position)
-  first_point  = results[position]
-  second_point = results[position + 1]
-  third_point  = results[position + 2]
-
-  return [(first_point + second_point + third_point), (position + 1)] if first_point == 10
-
-  point = first_point + second_point
-  point += third_point if point == 10
-  [point, (position + 2)]
-end
-
-results  = convert_to_numbers(ARGV[0])
+results  = ARGV[0].split(',').map { |result| result == 'X' ? 10 : result.to_i }
 score    = 0
 position = 0
 10.times do
-  point, position = calculate_point(results, position)
-  score += point
+  first_point, second_point, third_point = results[position, 3]
+  if first_point == 10
+    score += (first_point + second_point + third_point)
+    position += 1
+  elsif first_point + second_point == 10
+    score += (first_point + second_point + third_point)
+    position += 2
+  else
+    score += (first_point + second_point)
+    position += 2
+  end
 end
 
 puts score
